9d747bfa1438b7739f83d3036a0691e9558ec964
anjavM <molokomotywa@gmail.com>
2019-11-14 16:49:44 +0100

    initial commit, sample table

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Table from './Table';
+
+function App() {
+  return (
+    <div className="App container">
+      <h1 className="title">ProductsUp Task</h1>
+      <Table />
+    </div>
+  );
+}
+
+export default App;
